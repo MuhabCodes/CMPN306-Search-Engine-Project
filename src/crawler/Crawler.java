af6c5142f52817c2b1ddf682adb48f2c8da9d60d
Muhab <icecream10800@gmail.com>
2022-05-19 00:23:05 +0200

    Completed Crawler

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -20,21 +20,32 @@ import org.jetbrains.annotations.Nullable;
 
 public class Crawler implements Runnable {
     private static final int MIN_PAGES_TO_CRAWL = 5000;
-    private static final int MAX_LINK_DEPTH = 10;
     // These 2 path strings need to be updated to relative pathing
     private static final String SEED_FILE = "./seedURLs.txt";
     private static final String OUTPUT_DIRECTORY = "./html_docs/";
-    private static final int NUM_THREADS = 1;
+    private static final int NUM_THREADS = 10;
     // list of blocked extensions
     private static final List<String> BLOCKED_EXTENSIONS = new ArrayList<>(List.of(
             ".mp4",
             ".webm",
             ".gif",
             ".mov",
-            ".flv"
+            ".flv",
+            ".png",
+            ".jpg",
+            ".amv",
+            ".mpeg",
+            ".webp",
+            ".raw",
+            ".psd",
+            ".ppt",
+            ".bmp",
+            ".pdf"
             ));
 
     public static Queue<String> linksQueue = new LinkedList<>();
+    public static Queue<String> crawledQueue = new LinkedList<>();
+    public static HashMap<String, Boolean> visitedLinks = new HashMap<>();
     public static Queue<String> blockedUrls = new LinkedList<>();
     public static HashMap<String, Boolean> parsedRobots = new HashMap<>();
     public static Hashtable<String, String> urlToHtml = new Hashtable<>();
@@ -44,7 +55,11 @@ public class Crawler implements Runnable {
 
     public static @Nullable String normalize_url(String url) throws MalformedURLException {
         try {
-            URL url_object = new URL(url.trim());
+            url = url.trim();
+            while (url.endsWith("/"))
+                url = url.substring(0, url.length() - 1);
+
+            URL url_object = new URL(url);
             String query = (url_object.getQuery()) == null ? "" : "?" + url_object.getQuery();
             return url_object.getProtocol() + "://" + url_object.getHost() + url_object.getPath() + query;
         }
@@ -60,27 +75,33 @@ public class Crawler implements Runnable {
             if (parsedRobots.containsKey(robots_txt_url))
                 return;
             BufferedReader read = new BufferedReader(new InputStreamReader(new URL(robots_txt_url).openStream()));
-            String line = "";
+            String line;
             while ((line = read.readLine()) != null) {
                 if (line.toLowerCase().startsWith("user-agent")) {
                     if (line.contains("*")) {
                         while ((line = read.readLine()) != null) {
                             if (line.toLowerCase().startsWith("disallow")) {
                                 String disallow_url = line.split(":")[1].trim();
-                                blockedUrls.add(link.getProtocol() + "://" + link.getHost() + disallow_url);
+                                synchronized (blockedUrls) {
+                                    blockedUrls.add(link.getProtocol() + "://" + link.getHost() + disallow_url);
+                                }
                             }
                             else if (line.toLowerCase().startsWith("user-agent")) {
                                 break;
                             }
+                            /*
                             else if (line.toLowerCase().startsWith("sitemap")) {
-                                parse_sitemap_xml(line.split(":")[1].trim());
+                                parse_sitemap_xml(line.split(" ")[1].trim());
                             }
+                            */
                         }
                     }
                 }
+                /*
                 else if (line.toLowerCase().startsWith("sitemap")) {
-                    parse_sitemap_xml(line.split(":")[1].trim());
+                    parse_sitemap_xml(line.split(" ")[1].trim());
                 }
+                */
                 parsedRobots.put(robots_txt_url, true);
             }
         } catch (IOException | ArrayIndexOutOfBoundsException e) {
@@ -89,49 +110,57 @@ public class Crawler implements Runnable {
     }
 
     public static boolean is_url_allowed(String url) {
-        parse_robot(url);
-        String extension = FilenameUtils.getExtension(url);
-        for (String blocked_extension : BLOCKED_EXTENSIONS)
-            if (extension.equals(blocked_extension))
-                return false;
-        for (String blocked_url : blockedUrls)
-        {
-            try {
-                if ((new URL(url).getHost().equals(new URL(blocked_url).getHost()) && url.endsWith(extension)) || url.equals(blocked_url))
-                    return false;
-            }
-            catch (MalformedURLException e) {
-                System.out.println("Wrong URL:" + url);
-            }
-        }
-        return true;
-    }
-
-    public static boolean parse_sitemap_xml(String url) {
         try {
-            URL temp = new URL(url);
-            String sitemap = url + "sitemap.xml";
-            Document doc = Jsoup.connect(sitemap).get();
-            for (Element loc : doc.select("loc")) {
-                String absolute_link = loc.attr("abs:href");
-                absolute_link.replace("/sitemap.xml", "");
-                absolute_link.replace("/sitemaps.xml", "");
-                if (is_url_allowed(absolute_link) && !linksQueue.contains(absolute_link)) {
-                    linksQueue.add(normalize_url(absolute_link));
-                    System.out.println("Crawl Added: " + absolute_link);
-                    System.out.println("Current Size: " + linksQueue.size());
+            if (url == null)
+                return false;
+            if (!url.startsWith("http://") && !url.startsWith("https://"))
+                return false;
+
+            parse_robot(url);
+            String extension = FilenameUtils.getExtension(url);
+
+            for (String blocked_extension : BLOCKED_EXTENSIONS)
+                if (extension.equals(blocked_extension))
+                    return false;
+
+            var blockedUrlsCopy = new LinkedList<>(blockedUrls);
+            for (String blocked_url : blockedUrlsCopy)
+            {
+                try {
+                    if ((new URL(url).getHost().equals(new URL(blocked_url).getHost()) && url.endsWith(extension)) || url.equals(blocked_url))
+                        return false;
+                }
+                catch (MalformedURLException e) {
+                    System.out.println("Wrong URL:" + url);
                 }
             }
             return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void parse_sitemap_xml(String url) {
+        try {
+            URL temp = new URL(url);
+            String sitemap = url;
+            Document doc = Jsoup.connect(sitemap).get();
+            for (Element loc : doc.select("loc")) {
+                String absolute_link = loc.attr("abs:href");
+                absolute_link = absolute_link.replace("/sitemap.xml", "");
+                absolute_link = absolute_link.replace("/sitemaps.xml", "");
+                absolute_link = normalize_url(absolute_link);
+                add_to_crawled_queue(absolute_link);
+            }
         }
         catch (IOException e) {
             System.out.println("Couldn't parse sitemap.xml for url:" + url);
             System.out.println(e.getMessage());
         }
-        return false;
     }
 
-    public static boolean parse_url_html(String url) {
+    public static void parse_url_html(String url) {
         try {
             URL temp = new URL(url);
             String html = url + "/";
@@ -140,22 +169,14 @@ public class Crawler implements Runnable {
             Elements links = document.select("a[href]");
             for (Element link : links) {
                 String absolute_link = link.attr("abs:href");
-                if (is_url_allowed(absolute_link)) {
-                    if (!linksQueue.contains(absolute_link) && is_url_allowed(absolute_link)) {
-                        linksQueue.add(normalize_url(absolute_link));
-                        System.out.println("Crawl Added: " + absolute_link);
-                        System.out.println("Current Size: " + linksQueue.size());
-                    }
-                }
+                absolute_link = normalize_url(absolute_link);
+                add_to_crawled_queue(absolute_link);
             }
-
-            return true;
         }
         catch (IOException e) {
             System.out.println("couldn't parse url:" + url);
             System.out.println(e.getMessage());
         }
-        return false;
     }
 
     public static void download_html(String url) {
@@ -176,35 +197,113 @@ public class Crawler implements Runnable {
         }
     }
 
-    public static void SeedCrawler() throws java.io.IOException {
+    public static void add_to_crawled_queue(String url) {
+        if (is_url_allowed(url) && !visitedLinks.containsKey(url) && !linksQueue.contains(url) && !crawledQueue.contains(url)) {
+            synchronized (crawledQueue) {
+                crawledQueue.add(url);
+            }
+            System.out.println("Crawl Added: " + url);
+        }
+    }
+
+    public static void empty_crawled_queue() {
+        String appended_text = new String();
+        while (!crawledQueue.isEmpty()) {
+            String current_url = crawledQueue.poll();
+            if (!linksQueue.contains(current_url) && !visitedLinks.containsKey(current_url)) {
+                System.out.println("Emptying: " + current_url);
+                appended_text += current_url + "\n";
+                linksQueue.add(current_url);
+            }
+        }
+        append_to_file(appended_text, SEED_FILE);
+        System.out.println("Current Size: " + linksQueue.size());
+    }
+
+    public static void append_to_file(String data, String file_name) {
+        try {
+            File file = new File(file_name);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(data);
+            writer.close();
+        }
+        catch (IOException e) {
+            System.out.println("Couldn't append to file:" + file_name);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void rewrite_state_file() {
+        try {
+            File file = new File("./state.txt");
+            file.createNewFile();
+            int stop_counter = new Scanner(new File("./state.txt")).nextInt();
+            FileWriter writer = new FileWriter(file);
+            writer.write(Integer.toString(stop_counter + 1));
+            writer.close();
+        }
+        catch (IOException e) {
+            System.out.println("Couldn't rewrite state.txt");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void seed_crawler() throws java.io.IOException {
         File directory = new File(OUTPUT_DIRECTORY);
         if (!directory.exists())
             directory.mkdirs();
         File seed = new File(SEED_FILE);
         Scanner scanner = new Scanner(seed);
+        int counter = 0;
+        File state = new File("./state.txt");
+        if (state.createNewFile()) {
+            FileWriter writer = new FileWriter(state);
+            writer.write("0");
+            writer.close();
+        }
+        int stop_counter = new Scanner(state).nextInt();
         while (scanner.hasNextLine()) {
-            Crawler.linksQueue.add(normalize_url(scanner.nextLine()));
-            System.out.println("Crawl Added: " + normalize_url(scanner.nextLine()));
-            System.out.println("Current Size: " + linksQueue.size());
+            if (counter < stop_counter) {
+                visitedLinks.put(scanner.nextLine(), true);
+                counter += 1;
+                continue;
+            }
+            String url = normalize_url(scanner.nextLine());
+            linksQueue.add(url);
+            System.out.println("Crawl Added: " + url);
         }
     }
 
     @Override
     public void run() {
-        while (!linksQueue.isEmpty()) {
-            String url = linksQueue.remove();
-            if (url.contains("sitemap.xml") || url.contains("sitemaps.xml")) {
-                parse_sitemap_xml(url);
-                continue;
+        while (!linksQueue.isEmpty() && visitedLinks.size() < MIN_PAGES_TO_CRAWL) {
+            System.out.println("The Thread name is " + Thread.currentThread().getName());
+            String url = new String();
+            synchronized(linksQueue) {
+                url = linksQueue.poll();
             }
+            synchronized(visitedLinks) {
+                visitedLinks.put(url, false);
+            }
+            if (url == null)
+                continue;
+
+            if (urlToHtml.size() > MIN_PAGES_TO_CRAWL)
+                break;
+
             parse_url_html(url);
             download_html(url);
+            synchronized (visitedLinks) {
+                visitedLinks.replace(url, true);
+                rewrite_state_file();
+            }
+            empty_crawled_queue();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        // TODO Auto-generated method stub
-        SeedCrawler();
+        seed_crawler();
         Thread [] threads = new Thread[NUM_THREADS];
         for (int i = 0; i < NUM_THREADS; i++) {
             threads[i] = new Thread(new Crawler());
